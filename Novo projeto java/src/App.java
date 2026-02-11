80743b52d937742816f99424010c02170079e73c
isabellinc <bellinunes2@gmail.com>
2026-02-11 20:25:04 +0000

    Primeiro codigo java

--- Novo projeto java/src/App.java
+++ Novo projeto java/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Primeiro commit!");
+        System.out.println("Meu primeiro commit!");
     }
 }
